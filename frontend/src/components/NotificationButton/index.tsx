3f3954e094f0e7bb9214dfe3a9ae33a80813fca1
Guilherme Camboim <105326604+guilhermecamboim@users.noreply.github.com>
2022-11-20 21:27:05 -0300

    Send notification

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,9 +1,23 @@
+import axios from 'axios';
 import iconImg from '../../assets/notification-icon.svg';
+import { BASE_URL } from '../../utils/request';
 import './styles.css';
 
-export function NotificationButton() {
+interface INotificationProps {
+  saleId: number
+}
+
+export function NotificationButton({saleId}: INotificationProps) {
+
+  function handleClick(id: number){
+    axios(`${BASE_URL}/sales/${id}/notification`)
+    .then(response => {
+      console.log("Sucess")
+    })
+  }
+
   return (
-    <div className="dsmeta-red-btn">
+    <div className="dsmeta-red-btn" onClick={() => {handleClick(saleId)}}>
       <img src={iconImg} alt="Notificar"/>
     </div>
   )
